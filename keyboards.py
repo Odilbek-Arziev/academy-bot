e1f064131c50a4900962cd98f72ad6309f8c77bb
Odilbek Arziev <76895495+Odilbek-Arziev@users.noreply.github.com>
2021-08-16 11:52:36 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,5 +1,7 @@
 from telebot import types
+
 courses_keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=1)
+url_keyboard = types.InlineKeyboardMarkup()
 
 python = types.KeyboardButton(text='1️⃣ Python & Django')
 javascript = types.KeyboardButton(text='2️⃣ JavaScript & React')
@@ -9,6 +11,5 @@ wordpress = types.KeyboardButton(text='5️⃣ Wordpress')
 scratch = types.KeyboardButton(text='6️⃣ Scratch')
 courses_keyboard.add(python, javascript, smm, graphic_design, wordpress, scratch)
 
-url_keyboard = types.InlineKeyboardMarkup()
 url_button = types.InlineKeyboardButton(url='macademy.uz', text='Перейдите на наш сайт')
 url_keyboard.add(url_button)
